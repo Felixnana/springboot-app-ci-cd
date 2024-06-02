68a02efaea89126618ea9eb71ac8a6becfd0c140
ityourway <147886110+ityourway@users.noreply.github.com>
2024-06-02 16:38:38 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "springboot-app-s3-new-bucket"
+    bucket         = "springboot-app-s3-bucket"
     key            = "terraform_statefile"
     region         = "us-east-1"
     dynamodb_table = "springboot-app-dynamodb"
