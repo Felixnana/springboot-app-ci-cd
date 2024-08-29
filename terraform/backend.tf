9d87f5f558508d4fc3070ceec35538e1e5853fbb
F.ERIC NANA APPIAH <teenbigs1@gmail.com>
2024-08-29 12:44:06 +0100

    updated pipeline_list file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "stunnerz-springboot-app-bucket"
+    bucket         = "stunnerz-sprinboot-app-bucket"
     key            = "terraform_statefile"
     region         = "us-east-1"
     dynamodb_table = "stunnerz-springboot-app-bucket-DynamoDB"
